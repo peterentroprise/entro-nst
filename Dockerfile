98d3aa7f8a2568335c477ad3d93c36fd305a259c
Peter Arnold <peter@entroprise.com>
2020-08-12 11:18:19 -0400

    asd

--- Dockerfile
+++ Dockerfile
@@ -10,18 +10,6 @@ ENV PORT 8080
 
 RUN conda install -c conda-forge fastapi
 
-# haystack
-
-RUN pip install git+https://github.com/deepset-ai/haystack.git
-
-RUN pip install farm-haystack
-
-# wandb
-
-RUN pip install --upgrade wandb
-
-RUN wandb login 47d3a85229ed086517bbc6a1adb7995f989b089e
-
 #gcloud storage
 
 RUN echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg  add - && apt-get update -y && apt-get install google-cloud-sdk -y
@@ -34,6 +22,6 @@ ENV GOOGLE_APPLICATION_CREDENTIALS="service-account.json"
 
 RUN gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
 
-RUN gsutil -m cp -R gs://entro-haystack-models/sentence_bert /app
+# RUN gsutil -m cp -R gs://entro-haystack-models/sentence_bert /app
 
 COPY ./app /app
--- app/service/item_service.py
+++ app/service/item_service.py
@@ -1,59 +1,17 @@
 from models.item_model import Payload
 from models.item_model import Question
 
-from haystack import Finder
-from haystack.indexing.cleaning import clean_wiki_text
-from haystack.indexing.utils import convert_files_to_dicts, fetch_archive_from_http
-from haystack.reader.farm import FARMReader
-from haystack.reader.transformers import TransformersReader
-from haystack.utils import print_answers
-
-from haystack.database.elasticsearch import ElasticsearchDocumentStore
-from haystack.retriever.dense import DensePassageRetriever
-from haystack.retriever.dense import EmbeddingRetriever
-
 import pandas as pd
 import requests
 import uuid
 import torch
 
-# import wandb
-# wandb.init(project="entro-haystack")
-
 cuda_available = torch.cuda.is_available()
 
-document_store = ElasticsearchDocumentStore(host="35.188.203.27",
-                                            username="elastic",
-                                            password="qt5hfjmkmxtvlf4pw6qhlk6b",
-                                            index="faq",
-                                            text_field="answer",
-                                            embedding_field="question_emb",
-                                            embedding_dim=768,
-                                            excluded_meta_data=["question_emb"])
-
-retriever = EmbeddingRetriever(document_store=document_store, embedding_model="deepset/sentence_bert", use_gpu=cuda_available)
-
-finder = Finder(reader=None, retriever=retriever)
-
 def index_item(payload: Payload):
-
-    item = payload.event.data.new
-
-    df = pd.DataFrame({'id': item.id,'question': item.question, 'answer': item.answer, 'content': item.content}, index=[0])
-
-    # Get embeddings for our questions from the FAQs
-    questions = list(df["question"].values)
-    df["question_emb"] = retriever.embed_queries(texts=questions)
-
-    # Convert Dataframe to list of dicts and index them in our DocumentStore
-    docs_to_index = df.to_dict(orient="records")
-    document_store.write_documents(docs_to_index)
-
-    return item
+    print(payload)
+    return payload
 
 def ask_question(question: Question):
     print(question)
-
-    prediction = finder.get_answers_via_similar_questions(question=question.question, top_k_retriever=question.top_k_retriever)
-
-    return prediction
+    return question
