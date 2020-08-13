94ca025069a6a0f91954650953d06965ed5a3ced
Peter Arnold <peter@entroprise.com>
2020-08-13 11:21:54 -0400

    asd

--- app/service/video_service.py
+++ app/service/video_service.py
@@ -12,7 +12,7 @@ storage_client = storage.Client()
 bucket = storage_client.get_bucket(bucket_name)
 
 def create_upload_video(video: UploadFile = File(...)):
-    destination_blob_name = "test1"
+    destination_blob_name = f'{uuid.uuid4()}-{video.filename}'
     blob = bucket.blob(destination_blob_name)
     blob.upload_from_file(video.file)
     return f'Uploaded {video.filename} to "{bucket_name}" bucket.'
